ca31a9f62a511a1fa405445dc95d34868a617ab6
zhouxin <znz.zhouxin@gmail.com>
2017-04-30 11:10:45 +0800

    fix

--- app/models/concerns/model_concern.rb
+++ app/models/concerns/model_concern.rb
@@ -13,6 +13,11 @@ module ModelConcern
             attr_key = key.to_s.gsub('like_', '')
             result = result.where("#{self.name.tableize}.#{attr_key} ilike ?", "%#{value}%") if self.attribute_names.include?(attr_key.to_s)
           end
+
+          if key.to_s.include?('not_')
+            attr_key = key.to_s.gsub('not_', '')
+            result = result.where.not(attr_key.to_sym => value) if self.attribute_names.include?(attr_key.to_s)
+          end
         end
       end
       result
