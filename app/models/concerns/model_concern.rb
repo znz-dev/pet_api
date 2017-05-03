0fdff79b4ec3a0149974d0814e130770d5bcfc02
zhouxin <znz.zhouxin@gmail.com>
2017-05-03 10:40:27 +0800

    fix bug

--- app/models/concerns/model_concern.rb
+++ app/models/concerns/model_concern.rb
@@ -8,16 +8,28 @@ module ModelConcern
         keys = params.keys
         keys.each do |key|
           value = params[key]
-          result = result.where(key.to_sym => value) if self.attribute_names.include?(key.to_s)
-          if key.to_s.include?('like_')
-            attr_key = key.to_s.gsub('like_', '')
-            result = result.where("#{self.name.tableize}.#{attr_key} ilike ?", "%#{value}%") if self.attribute_names.include?(attr_key.to_s)
+
+          if value.present?
+            result = result.where(key.to_sym => value) if self.attribute_names.include?(key.to_s)
+            if key.to_s.include?('like_')
+              attr_key = key.to_s.gsub('like_', '')
+              result = result.where("#{self.name.tableize}.#{attr_key} ilike ?", "%#{value}%") if self.attribute_names.include?(attr_key.to_s)
+            end
+
+            if key.to_s.include?('not_')
+              attr_key = key.to_s.gsub('not_', '')
+              result = result.where.not(attr_key.to_sym => value) if self.attribute_names.include?(attr_key.to_s)
+            end
+          else
+            result = result.where(key.to_sym => nil) if self.attribute_names.include?(key.to_s)
+
+            if key.to_s.include?('not_')
+              attr_key = key.to_s.gsub('not_', '')
+              result = result.where.not(attr_key.to_sym => nil) if self.attribute_names.include?(attr_key.to_s)
+            end
+
           end
 
-          if key.to_s.include?('not_')
-            attr_key = key.to_s.gsub('not_', '')
-            result = result.where.not(attr_key.to_sym => value) if self.attribute_names.include?(attr_key.to_s)
-          end
         end
       end
       result
