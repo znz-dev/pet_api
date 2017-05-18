699ce1395cef2247460ca7e19b609136f960b4f6
zhouxin <znz.zhouxin@gmail.com>
2017-05-18 10:36:51 +0800

    fix

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module PetApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
 
